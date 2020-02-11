be22718824f1d742fdeba22b839472e2e282c96f
tehvicke <vikda574@student.liu.se>
2020-02-11 10:40:53 +0100

    minor layout updates

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div className="portfolio-app">
-      <img src ="viktor.png" alt="Portrait of Viktor" className="portrait"/>
-        <h1>Viktor Dahl</h1>
-        <h3>Full-stack developer | Ex-Mgmt Consultant | Freelancer | Dreamer | Geek | M.Sc.</h3>
-        <br />
-      <div className="info-box">
-        <div className="links">
-          <a href="https://github.com/tehvicke" target="_blank">
-            <img src="/icons/GitHub-Mark-Light-64px.png" alt="Github logo"/>
+    <div className='portfolio-app'>
+      <img src='viktor.png' alt='Portrait of Viktor' className='portrait' />
+      <h1>Viktor Dahl</h1>
+      <h3>Full-stack developer | Ex-Mgmt Consultant | Freelancer</h3>
+      <br />
+      <div className='info-box'>
+        <div className='links'>
+          <a href='https://github.com/tehvicke' target='_blank' rel='noopener noreferrer'>
+            <img src='/icons/GitHub-Mark-Light-64px.png' alt='Github logo' />
             <p>Github</p>
           </a>
-          <a href="https://www.linkedin.com/in/viktordahl/" target="_blank">
-            <img src="/icons/LinkedIn.png" alt="LinkedIn logo"/>
+          <a href='https://www.linkedin.com/in/viktordahl/' target='_blank' rel='noopener noreferrer'>
+            <img src='/icons/LinkedIn.png' alt='LinkedIn logo' />
             <p>LinkedIn</p>
           </a>
-          <a href="https://www.facebook.com/tehvicke" target="_blank">
-            <img src="/icons/facebook-3-64.png" alt="Facebook logo"/>
+          <a href='https://www.facebook.com/tehvicke' target='_blank' rel='noopener noreferrer'>
+            <img src='/icons/facebook-3-64.png' alt='Facebook logo' />
             <p>Facebook</p>
           </a>
-          <a href="https://medium.com/@tehvicke" target="_blank">
-            <img src="/icons/medium.png" alt="Medium logo"/>
+          <a href='https://medium.com/@tehvicke' target='_blank' rel='noopener noreferrer'>
+            <img src='/icons/medium.png' alt='Medium logo' />
             <p>Medium</p>
           </a>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
